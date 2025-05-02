42ddc637503b7f30574a1537d3a5865388d31e71
ChethanRaj13 <chethanrajjee@gmail.com>
2025-05-02 05:46:52 +0530

    Added Day-5.dart

---
+++ Day-5.dart
@@ -0,0 +1,20 @@
+main(){
+  var Num = 10.0;
+
+  print(Num + 10);
+  print(Num - 10);
+  print(Num * 10);
+  print(Num / 10);
+  print(Num % 10);
+
+  Num += 10;
+  print(Num);
+  Num -= 10;
+  print(Num);
+  Num *= 10;
+  print(Num);
+  Num /= 10;
+  print(Num);
+  Num %= 10;
+  print(Num);
+}
